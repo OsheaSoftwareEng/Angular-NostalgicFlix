81ad9c81e44896308d97f836b263604ecd3e20a9
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-11-30 13:26:20 -0500

    profile ts file update and delelte user functions

---
+++ src/app/profile-page/profile-page.component.ts
@@ -0,0 +1,68 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service'
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+type User = { _id?: string, Username?: string, Password?: string, Email?: string, FavoriteMovies?: [] }
+
+@Component({
+  selector: 'app-profile-page',
+  templateUrl: './profile-page.component.html',
+  styleUrls: ['./profile-page.component.scss']
+})
+export class ProfilePageComponent implements OnInit {
+  user: User = {};
+
+  @Input() userData = { Username: '', Password: '', Email: '' };
+  
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public snackBar: MatSnackBar,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const user = this.getUser();
+
+    if (!user._id) {
+      this.router.navigate(['welcome']);
+      return;
+    }
+
+    this.user = user;
+    this.userData = {
+      Username: user.Username || "",
+      Email: user.Email || "",
+      Password: ""
+    }
+  }
+
+  getUser(): User {
+    return JSON.parse(localStorage.getItem('user') || '{}');
+  }
+
+  updateUser(): void {
+    this.fetchApiData.updateUser(this.userData).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(result))
+      this.user = result;
+      this.snackBar.open('user updated!', 'OK', {
+        duration: 2000
+      })
+    })
+  }
+
+  deleteUser(): void {
+    if (confirm('Are you sure you want to delete your account? This action cannnot be undone.')) {
+      this.router.navigate(['welcome']).then(() => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
+        this.fetchApiData.deleteUser().subscribe(result =>{
+          console.log('Resolution:', result);
+        })
+        this.snackBar.open('Your account has been deleted.', 'OK', {
+          duration: 3000
+        });
+      })}
+    
+  }
+}
