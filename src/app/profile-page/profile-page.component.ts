3cd602d863d861b8d93ec30a612411d8180b721f
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-12-05 12:07:37 -0500

    added matdialog and favoritemovies logic

--- src/app/profile-page/profile-page.component.ts
+++ src/app/profile-page/profile-page.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service'
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MovieInfoComponent } from '../movie-info/movie-info.component';
 
 type User = { _id?: string, Username?: string, Password?: string, Email?: string, FavoriteMovies?: [] }
 
@@ -11,14 +13,18 @@ type User = { _id?: string, Username?: string, Password?: string, Email?: string
   styleUrls: ['./profile-page.component.scss']
 })
 export class ProfilePageComponent implements OnInit {
-  user: User = {};
+
+  user: any = {};
+
+  FavoriteMovies: any[] = [];
 
   @Input() userData = { Username: '', Password: '', Email: '' };
   
   constructor(
     public fetchApiData: FetchApiDataService,
     public snackBar: MatSnackBar,
-    public router: Router
+    public router: Router,
+    public dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
@@ -35,11 +41,17 @@ export class ProfilePageComponent implements OnInit {
       Email: user.Email || "",
       Password: ""
     }
+
+    this.fetchApiData.getAllMovies().subscribe((response: any) => {
+      this.FavoriteMovies = response.filter((m: { _id: any; }) => this.user.FavoriteMovies.indexOf(m._id) >= 0);
+    });
   }
 
   getUser(): User {
     return JSON.parse(localStorage.getItem('user') || '{}');
   }
+
+ 
 
   updateUser(): void {
     this.fetchApiData.updateUser(this.userData).subscribe((result) => {
@@ -63,6 +75,62 @@ export class ProfilePageComponent implements OnInit {
           duration: 3000
         });
       })}
-    
+  }
+  openGenreInfo(genre: any): void {
+    this.dialog.open(MovieInfoComponent,{
+      data: {
+        title: genre.Name,
+        content: genre.Description
+      }
+    })
+  }
+
+  openDirectorInfo(director: any): void {
+    this.dialog.open(MovieInfoComponent,{
+      data: {
+        title: director.Name,
+        content: director.Bio
+      }
+    })
+  }
+
+  openMovieDescription(description: string): void {
+    this.dialog.open(MovieInfoComponent,{
+      data: {
+        title: 'Description',
+        content: description
+      }
+    })
+  }
+
+  openMoviePreview(movie: any): void {
+    this.dialog.open(MovieInfoComponent,{
+      data: {
+        title: movie.name,
+        content: movie.MovieEmbed
+      }
+    })
+  }
+
+  isFavorite(id: string): boolean {
+    return this.fetchApiData.isFavoriteMovie(id)
+  }
+
+  removeFavoriteMovie(id: string): void {
+    this.fetchApiData.removeMovieFavorites(id).subscribe(() => {
+      this.snackBar.open('removed from favorites', 'OK', {
+        duration: 2000
+      })
+    });
+  }
+
+  addFavoriteMovie(id: string): void {
+    this.fetchApiData.addMovieFavorites(id).subscribe(() => {
+      this.snackBar.open('added to favorites', 'OK', {
+        duration: 2000
+      })
+    });
   }
 }
+
+
