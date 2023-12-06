fd6354e5eb5700374bc61882f6dbe5eef916e9cf
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-12-06 11:38:11 -0500

    updated snackbar to display account created

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -34,14 +34,9 @@ registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
   // Logic for a successful user registration goes here! (To be implemented)
      this.dialogRef.close(); // This will close the modal on success!
-     this.snackBar.open(result, 'OK', {
+     this.snackBar.open('Your account was successfully created!', 'OK', {
         duration: 2000
      });
-    }, (result) => {
-      this.snackBar.open('Your account was successfully created!', 'OK', {
-        duration: 2000
-      });
-    });
-   }
-  }
+    }
+)}}
 
